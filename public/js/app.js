868477a56d00552e75f3b1afe925da9b51d9481d
Purushottam Gupta <guptapurushottam123@gmail.com>
2021-01-28 22:23:47 +0530

    modification

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,46 @@
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.getElementById('message-1')
 const messageTwo = document.getElementById('message-2')
+const btn2 = document.getElementById('btn-2')
 
-weatherForm.addEventListener('submit',(e) => {
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-
+    search.focus()
     const location = search.value
     messageOne.textContent = 'loading data...'
     messageTwo.textContent = ''
-    fetch('/weather?address='+ location).then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-            messageOne.textContent = data.error
-        } else {
-            messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast
-        }
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
     })
 })
+btn2.addEventListener('click', () => {
+    search.value = ''
+    search.focus()
+    if (!navigator.geolocation)
+           return alert('Geolocation is not supported in your browser')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const latitude = position.coords.latitude
+        const longitude = position.coords.longitude
+        messageOne.textContent = 'loading data...'
+        messageTwo.textContent = ''
+        fetch(`/weather?address=${latitude},${longitude}`).then((response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    messageOne.textContent = data.error
+                } else {
+                    messageOne.textContent = data.location
+                    messageTwo.textContent = data.forecast
+                }
+            })
+        })
+    })
 })
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,23 +1,33 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZnVsbG1ldGFsNjQiLCJhIjoiY2tqMWkyZ291MnVtZzJ1bW0zaHRsYzNleSJ9.4kKwut1sh3K5L-CHUMIAww&limit=1'
-
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect location services', undefined)
-        } else if (body.features.length === 0) {
-            callback('Unable to find given location, please try with different search', undefined)
-        } else {
-            const data =
-            {
-                latitude: body.features[0].center[1],//sencond one in center is latitude
-                longitude: body.features[0].center[0],//first one in center is longitude
-                location: body.features[0].place_name
-            }
-            callback(undefined, data)
+    const res = address.split(',')
+    if (Number(res[0])) {
+        const data = {
+            latitude: res[0],
+            longitude: res[1],
+            location: 'Your region is'
         }
-    })
+        callback(undefined, data)
+    } else {
+        const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZnVsbG1ldGFsNjQiLCJhIjoiY2tqMWkyZ291MnVtZzJ1bW0zaHRsYzNleSJ9.4kKwut1sh3K5L-CHUMIAww&limit=1'
+
+        request({ url, json: true }, (error, { body }) => {
+            if (error) {
+                callback('Unable to connect location services', undefined)
+            } else if (body.features.length === 0) {
+                callback('Unable to find given location, please try with different search', undefined)
+            } else {
+                const data =
+                {
+                    latitude: body.features[0].center[1],//sencond one in center is latitude
+                    longitude: body.features[0].center[0],//first one in center is longitude
+                    location: body.features[0].place_name + ' is'
+                }
+                callback(undefined, data)
+            }
+        })
+    }
 }
 
 module.exports = geocode
